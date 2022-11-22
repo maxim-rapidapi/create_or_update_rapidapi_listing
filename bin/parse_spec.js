f148318c17d41fddf4dd323770e148d89efacc0e
Maxim Burgerhout <maxim@wzzrd.com>
2022-11-22 11:38:16 +0100

    error handing for spec parsing

--- bin/parse_spec.js
+++ bin/parse_spec.js
@@ -1,10 +1,16 @@
+const { SpecParsingError } = require('./errors')
+
 /**
  * Return the version field of a spec file
  * @param {string} spec The contents of a spec file
  * @returns {string} The contents of the version field in the info block
  */
 function api_version_from_spec(spec) {
-    return spec.info.version
+    if (spec.info.version == undefined) {
+        throw new SpecParsingError("No property 'version' in spec")
+    } else {
+        return spec.info.title
+    }
 }
 
 /**
@@ -13,7 +19,11 @@ function api_version_from_spec(spec) {
  * @returns {string} The contents of the title field in the info block
  */
 function api_name_from_spec(spec) {
-    return spec.info.title
+    if (spec.info.title == undefined) {
+        throw new SpecParsingError("No property 'title' in spec")
+    } else {
+        return spec.info.title
+    }
 }
 
 /**
@@ -22,7 +32,11 @@ function api_name_from_spec(spec) {
  * @returns {string} The contents of the description field in the info block
  */
 function api_description_from_spec(spec) {
-    return spec.info.description
+    if (spec.info.description == undefined) {
+        throw new SpecParsingError("No property 'description' in spec")
+    } else {
+        return spec.info.title
+    }
 }
 
 module.exports = { api_version_from_spec, api_name_from_spec, api_description_from_spec }
