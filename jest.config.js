4158c2c23e599c2457fb6f6f2ad608b3124867e1
Maxim Burgerhout <maxim@wzzrd.com>
2022-11-23 10:36:21 +0100

    make jest work properly
    add .test.env file and dotenv.config
    add mock env vars to .test.env

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    setupFiles: [
+        "<rootDir>/tests/dotenv-config.js"
+    ],
+    roots: ['<rootDir>'],
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/tests/*.test.js'],
+    silent: true
+}
