d87b9dd7f977a62bc21e13395f44fa368f8ae07c
Maxim Burgerhout <maxim@wzzrd.com>
2022-11-24 14:00:23 +0100

    initial tests for set_created_version_as_active

--- tests/set_created_version_as_active.test.js
+++ tests/set_created_version_as_active.test.js
@@ -1,5 +1,25 @@
 const {
     set_created_version_as_active,
 } = require('../bin/set_created_version_as_active')
+const graphql = require('graphql-request')
+const nock = require('nock')
 
-test('test placeholder', async () => {})
+const res = {
+    data: {
+        updateApiVersion: [
+            {
+                id: 'testVersionId',
+            },
+        ],
+    },
+}
+
+test('handling set_created_version_as_active reponse', async () => {
+    const c = new graphql.GraphQLClient(
+        'https://platform-graphql.p.rapidapi.com'
+    )
+    nock('https://platform-graphql.p.rapidapi.com').post('/').reply(200, res)
+    expect(
+        await set_created_version_as_active('api_version_id', c)
+    ).toBeUndefined()
+})
